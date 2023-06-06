c28436236ceff41d0aae56f99ed381e5fe388594
Dani176343 <daniel.pereira@edu.abrantes.pt>
2023-06-06 15:43:55 +0100

    Update

--- Industries Manager/PMenu.vb
+++ Industries Manager/PMenu.vb
@@ -58,4 +58,8 @@
         Me.TableAdapterManager.UpdateAll(Me.Industries_DanDataSet)
 
     End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+    End Sub
 End Class
