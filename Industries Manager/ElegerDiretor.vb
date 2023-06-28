993b7453bd0a027a2bb33bd1214815e74365ea2a
Dani176343 <daniel.pereira@edu.abrantes.pt>
2023-06-28 22:06:32 +0100

    Update

--- Industries Manager/ElegerDiretor.vb
+++ Industries Manager/ElegerDiretor.vb
@@ -6,7 +6,33 @@
         Me.FuncionariosTableAdapter.Fill(Me.Industries_DanDataSet.Funcionarios)
         'TODO: This line of code loads data into the 'Industries_DanDataSet.Diretores_de_Departamentos' table. You can move, or remove it, as needed.
         Me.Diretores_de_DepartamentosTableAdapter.Fill(Me.Industries_DanDataSet.Diretores_de_Departamentos)
+    End Sub
 
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Diretores_de_DepartamentosBindingSource.AddNew()
+        Diretores_de_DepartamentosBindingSource.Current("ID_Departamento") = DepIDLoc
+        Diretores_de_DepartamentosBindingSource.Current("DDD") = TextBox1.Text
+        Diretores_de_DepartamentosBindingSource.Current("ID_Funcionario") = DepIDLoc
+        Diretores_de_DepartamentosBindingSource.Current("Funcoes") = DepIDLoc
+        Diretores_de_DepartamentosBindingSource.Current("DDC") = Today
+        If CheckBox1.Checked Then
+            Diretores_de_DepartamentosBindingSource.Current("DDF") = DateTimePicker1.Value
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            DateTimePicker1.Enabled = True
+        Else
+            DateTimePicker1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ElegerDiretor_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
         Label1.Text = "Escolha o novo Diretor do departamento de " & DepIDLoc
 
         If InfoUser.UserAdm Then
@@ -27,25 +53,5 @@
 
         DateTimePicker1.MinDate = Today.AddDays(7)
         DateTimePicker1.Value = Today.AddYears(1)
-    End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Diretores_de_DepartamentosBindingSource.AddNew()
-        Diretores_de_DepartamentosBindingSource.Current("ID_Departamento") = DepIDLoc
-        Diretores_de_DepartamentosBindingSource.Current("DDD") = TextBox1.Text
-        Diretores_de_DepartamentosBindingSource.Current("ID_Funcionario") = DepIDLoc
-        Diretores_de_DepartamentosBindingSource.Current("Funcoes") = DepIDLoc
-        Diretores_de_DepartamentosBindingSource.Current("DDC") = Today
-        Diretores_de_DepartamentosBindingSource.Current("DDF") = 
-    End Sub
-
-    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-        If CheckBox1.Checked Then
-
-        End If
     End Sub
 End Class
