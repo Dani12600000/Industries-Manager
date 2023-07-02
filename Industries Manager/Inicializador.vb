206771de8bff474645875219de9fb31a0742f84d
Dani12600000 <42302717+Dani12600000@users.noreply.github.com>
2023-07-02 19:06:49 +0100

    Update

--- Industries Manager/Inicializador.vb
+++ Industries Manager/Inicializador.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports System.Net
 Public Class Inicializador
     Private Sub Inicializador_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If File.Exists("C:\Industries Dan_PAP\Industries Dan.mdb") Then
@@ -12,6 +13,19 @@ Public Class Inicializador
                 Login.Show()
             End If
             Me.Close()
+        Else
+            ' Crie uma instância de WebClient
+            Dim webClient As New WebClient()
+
+            ' Especifique o local de destino para o arquivo
+            Dim destino As String = "C:\Industries Dan_PAP\Industries Dan.mdb"
+
+            ' Faça o download dos arquivos usando o link direto
+            webClient.DownloadFile("https://github.com/Dani12600000/Industries-Manager/raw/master/Industries%20Dan.mdb", destino)
+            webClient.DownloadFile("", destino)
+
+            ' Libere os recursos do WebClient
+            webClient.Dispose()
         End If
     End Sub
 End Class
