0a725bcce1dd9156e8c91a2500d07b33ccb6f76d
Dani12600000 <42302717+Dani12600000@users.noreply.github.com>
2023-07-04 22:25:58 +0100

    Update

--- Industries Manager/Inicializador.vb
+++ Industries Manager/Inicializador.vb
@@ -1,9 +1,5 @@
 ﻿Imports System.IO
 Public Class Inicializador
-    Private Sub Inicializador_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
     Private Sub Inicializador_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
         If File.Exists("C:\Industries Dan_PAP\Industries Dan.mdb") Then
             'TODO: esta linha de código carrega dados na tabela 'Industries_DanDataSet.Funcionarios'. Você pode movê-la ou removê-la conforme necessário.
